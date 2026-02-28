b62048123149377d595f384927ad0e2ae9557c46
JoshNotChicken <joshloo619@gmail.com>
2026-02-28 23:39:09 +0700

    Add files via upload

---
+++ component/navbar.php
@@ -0,0 +1,16 @@
+<body>
+    <nav>
+        <div class="nav">
+            <p>
+                SchoDy Hub
+            </p>
+
+            <ul>
+                <li><a href="./index.php">home</a></li>
+                <li><a href="./login.php">Login</a></li>
+
+            </ul>
+        </div>
+    </nav>
+
+</body>
